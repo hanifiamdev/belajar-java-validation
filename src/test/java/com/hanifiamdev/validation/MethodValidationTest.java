f7f7654169d8c0af5738b78c9df6ac91bdf6a21b
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-03 23:30:16 +0700

    Method Validation

---
+++ src/test/java/com/hanifiamdev/validation/MethodValidationTest.java
@@ -0,0 +1,47 @@
+package com.hanifiamdev.validation;
+
+import jakarta.validation.ConstraintViolation;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.Set;
+
+public class MethodValidationTest extends AbstractValidatorTest{
+
+    @Test
+    void testSayHello() throws NoSuchMethodException {
+
+        Person person = new Person();
+
+        String name = ""; // ini sengaja supaya kena validasi blank untuk parameter method sayHello
+
+        Method method = Person.class.getMethod("sayHello", String.class);
+
+        Set<ConstraintViolation<Person>> violations = executableValidator
+                .validateParameters(person, method, new Object[]{name});
+        for (ConstraintViolation<Person> violation : violations) {
+            System.out.println(violation.getPropertyPath());
+            System.out.println(violation.getMessage());
+            System.out.println("============");
+        }
+    }
+
+    @Test
+    void testFullName() throws NoSuchMethodException {
+
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("");
+
+        String returnValue = person.fullName();
+
+        Method method = Person.class.getMethod("fullName");
+        Set<ConstraintViolation<Person>> violations = executableValidator.validateReturnValue(person, method, returnValue);
+        for (ConstraintViolation<Person> violation : violations) {
+            System.out.println(violation.getPropertyPath());
+            System.out.println(violation.getMessage());
+            System.out.println("============");
+        }
+
+    }
+}
